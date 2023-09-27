eb0e7ab2bddbe4cca8e9d99f50e5c4bb715d78fb
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-09-27 18:49:32 -0400

    ♻ Makes Module Exports Explicit

--- src/poshy-coreutils-ish.psm1
+++ src/poshy-coreutils-ish.psm1
@@ -7,7 +7,6 @@ foreach ($item in Get-ChildItem -Path $PSScriptRoot -Filter "*.ps1" -File) {
     [string] $scriptFileContent = Get-Content -Path $item.FullName -Raw
     if (-not ($scriptFileContent -like "*TODO: Implement*")) {
         . $item.FullName
+        Export-ModuleMember -Function $item.BaseName
     }
 }
-
-Export-ModuleMember -Function *
